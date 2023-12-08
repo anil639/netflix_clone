8849ffc834f3c28c480c834da36fe0ec85963039
Anil kumar nayak <anilkumarnayak639@gmail.com>
2023-12-08 12:18:21 +0530

    Login Page updated.

--- netflix_client/src/App.js
+++ netflix_client/src/App.js
@@ -1,11 +1,31 @@
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import React, { useEffect, useState } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { Routes, Route, Navigate } from "react-router-dom";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
+import Home from "./Pages/Home";
+
+const auth = getAuth();
 function App() {
+  //to check user authentication status
+  const [user, setUser] = useState(null);
+  //for keep track of users status
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
   return (
     <div className="App">
       <Routes>
+        <Route path="/" element={user ? <Home /> : <Navigate to="/login" />} />
         <Route path="/signUp" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
